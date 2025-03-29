47f56398f503070a14b7c0d40ec623ba119a49e4
harry.bui <mr.flooo1230@gmail.com>
2025-03-29 14:53:38 +0700

    update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['crypto-chart-web.onrender.com']
+    allowedHosts: ['all']
   }
 })
